74678b41d5cf3a12e3e34051f7d47d501964f9f9
KarimIsaac <90776600+KarimIsaac@users.noreply.github.com>
2024-05-20 14:17:44 +0200

    create message controller

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,9 +1,31 @@
+import Chat from "../models/chat.js"
+import Message from "../models/message.js"
 export const sendMessage = async(req, res) =>{
     try {
        const {message}= req.body;
-       const {id} = req.params;
-       const senderId = req.userId
-        
+       const {id: receiverId} = req.params;
+       const senderId = req.user._id
+
+       let Chat = await Chat.findOne({
+        members: {$all : [senderId, receiverId]},
+       })
+      if(!Chat){
+            Chat = await Chat.create({
+                members: [senderId, receiverId],
+                messages: [{senderId, message}]
+            })
+            }
+      const newMessage = new Message({
+        senderId,
+        receiverId, 
+        message
+     })
+     if (newMessage){
+        Chat.messages.push(newMessage._id)
+        //await Chat.save()
+        return res.status(200).json({message: "Message sent"})
+     
+     }
     } catch (error) {
         console.error(error);
         return res.status(500).json({error: "Server error"});
--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { sendMessage } from "../controllers/message.controller.js";
+import  secureRoute  from "../middleware/secureRoute.js";
 const router = express.Router();
 
 router.post("/send/id",secureRoute, sendMessage) 
